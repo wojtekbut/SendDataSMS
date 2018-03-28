3dde21ae97f92b42ac5122abf412d3812c10dd8c
wojtek <wojtekbut@gmail.com>
2018-03-29 01:52:24 +0200

    comit 1

--- app/src/main/java/com/wojtek/butrym/senddatasms/MainActivity.java
+++ app/src/main/java/com/wojtek/butrym/senddatasms/MainActivity.java
@@ -2,7 +2,11 @@ package com.wojtek.butrym.senddatasms;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
@@ -14,7 +18,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements View.OnClickListener{
 
     EditText numer;
-    Button zadzwon, gdzie;
+    Button zadzwon, gdzie, kontakt;
     Context context;
 
 
@@ -26,8 +30,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
         numer = (EditText) findViewById(R.id.numerTel);
         zadzwon = (Button) findViewById(R.id.zadzwon);
         gdzie = (Button) findViewById(R.id.gdzie);
+        kontakt = (Button) findViewById(R.id.kontakt);
         zadzwon.setOnClickListener(this);
         gdzie.setOnClickListener(this);
+        kontakt.setOnClickListener(this);
 
         Log.e("onCreate", "koniec");
     }
@@ -35,7 +41,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         Log.e("onClick", "startuje");
-        if (numer.getText().toString().equals("")){
+        if (numer.getText().toString().equals("") && (view.getId() != R.id.kontakt)){
             Log.e("if","Pusty");
             Toast toast = Toast.makeText(context,"Podaj numer",Toast.LENGTH_SHORT);
             toast.show();
@@ -58,11 +64,30 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 byte[] smsBody2 = "Gdzie jestes?".getBytes();
                 smsManager.sendDataMessage(phoneNumber, null, port, smsBody2, null, null);
                 break;
+            case R.id.kontakt:
+                Intent pickContactIntent = new Intent( Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI );
+                pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(pickContactIntent, 1);
             default:
                 Log.e("switch","koniec");
-
         }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            String phoneNo = null;
+            Uri uri = data.getData();
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
 
+            if (cursor.moveToFirst()) {
+                int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                phoneNo = cursor.getString(phoneIndex);
+            }
+
+            cursor.close();
+            numer.setText(phoneNo);
+        }
     }
 }
